8c63097f83084f050d3c28b0df2ba760052e8672
YayoReynoso <eduardo_3reynoso@hotmail.com>
2022-07-23 16:17:41 -0700

    Assignment 2

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ let nonImportantIconClass = "fa-solid fa-star";
 
 let isImportant = false;
 
+
 function Important(){
 
     if(!isImportant){
@@ -23,11 +24,39 @@ function Important(){
     
 }
 
+let Hide = true;
+
+function hideForm(){
+    if(!Hide){
+        console.log("Hiding...");
+        $("#hide-button").hide();
+        $("#task-form").hide();
+       
+        $("#add-button").show();
+        Hide = true
+    }else{
+        console.log("Showing...");
+        $("#hide-button").show();
+        $("#task-form").show();
+        
+        $("#add-button").hide();
+
+        Hide = false;
+    }
+}
+
 
 function init(){
 
     $("#icon-important").click(Important);
     $("#icon-nonImportant").click(Important);
+
+
+    $("#hide-button").hide();
+    $("#task-form").hide();
+
+    $("#add-button").click(hideForm);
+    $("#hide-button").click(hideForm);
 
 }
 
